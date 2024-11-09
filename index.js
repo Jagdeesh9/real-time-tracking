37b1219877d175d91024ebde03eee043c870cefd
unknown <jagdeeshbhandari05@gmail.com>
2024-11-09 12:23:58 +0530

    static

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const io = socketio(server)
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
     res.render('index');
